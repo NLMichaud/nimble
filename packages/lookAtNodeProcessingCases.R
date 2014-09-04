03c3a091d1fcd6f16c91ba7165bc8a50bc991288
perrydv <perry@Perry-de-Valpines-MacBook-Pro.local>
2014-09-04 09:53:58 -0700

    updated .gitignore and made lookAtNodeProcessingCases.R

---
+++ packages/lookAtNodeProcessingCases.R
@@ -0,0 +1,26 @@
+source('loadAllCode.R')
+
+## Notes and examples as we plan to re-write some of the node processing and graph inspection functions
+
+## example of looking at newSetupCode
+
+m1c <- modelCode({ x ~ dnorm(mu, 1); mu ~ dnorm(0, 2)})
+m1 <- nimbleModel(m1c)
+Cm1 <- compileNimble(m1)
+
+m1mcmcSpec <- MCMCspec(m1)
+m1mcmc <- buildMCMC(m1mcmcSpec)
+Cm1mcmc <- compileNimble(m1mcmc, project = m1)
+
+## looking inside things:
+m1proj <- getNimbleProject(Cm1mcmc)
+ls(m1proj)
+ls(m1proj$nimbleFunctions) ## the "x_*" and "mu_*" are from the model.  the "nfRefClass_*" are from the mcmc.
+ls(m1proj$nfCompInfos) ## these are the comilation information objects
+class(m1proj$nfCompInfos[['mu_L2_UID5']]$nfProc)
+m1proj$nfCompInfos[['mu_L2_UID5']]$nfProc$newSetupCode
+m1proj$nfCompInfos[['mu_L2_UID5']]$nfProc$newSetupCodeOneExpr
+
+m1proj$nfCompInfos[['nfRefClass32']]$nfProc$newSetupCode
+m1proj$nfCompInfos[['nfRefClass32']]$nfProc$newSetupCodeOneExpr
+
